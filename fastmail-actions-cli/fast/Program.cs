27ad58713ce3a4030e719f4754ce27a6d27dba8f
Ronan Lamour <bold.spot3022@lamour.bzh>
2023-07-13 15:37:26 +0200

    job done.

---
+++ fastmail-actions-cli/fast/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using fast.Services;
+
+// ask user for description
+Console.WriteLine("Description pour l'email:");
+var description = Console.ReadLine();
+var service = new FastmailService();
+if (description != null)
+{
+    var email = await service.SetMaskedEmail(description);
+    Console.WriteLine($"Email créé : {email}");
+    // copy email to clipboard
+    TextCopy.ClipboardService.SetText(email);
+    Console.WriteLine("L'email a été copié dans le presse-papier.");
+}
+else
+{
+    Console.WriteLine("Veuillez entrer une description.");
+}
+
+// wait for user input
+Console.WriteLine("Appuyez sur une touche pour quitter.");
+Console.ReadKey();
---
+++ fastmail-actions-cli/fast/Services/FastmailService.cs
@@ -0,0 +1,91 @@
+﻿using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json.Nodes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace fast.Services;
+
+public class FastmailService
+{
+    private readonly string _token = "#APIKEY";
+    private readonly string _sessionUrl = "jmap/session";
+
+    public FastmailService()
+    {
+    }
+
+    public async Task<string> SetMaskedEmail(string description)
+    {
+        try
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://api.fastmail.com/");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _token);
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            
+            var result = await client.GetAsync(_sessionUrl);
+            result.EnsureSuccessStatusCode();
+            var response = await result.Content.ReadAsStringAsync();
+            
+            var json = JsonNode.Parse(response);
+            var accountId = json?["primaryAccounts"]?["https://www.fastmail.com/dev/maskedemail"]?.ToString();
+            var apiUrl = json?["apiUrl"]?.ToString();
+            
+            //Console.WriteLine($"Account Id: {accountId}, API Url: {apiUrl}");
+            
+            var requestJObj = new JObject
+            {
+                new JProperty("using",
+                    new JArray { "https://www.fastmail.com/dev/maskedemail", "urn:ietf:params:jmap:core" }),
+                new JProperty("methodCalls",
+                    new JArray
+                    {
+                        new JArray
+                        {
+                            "MaskedEmail/set",
+                            new JObject
+                            {
+                                ["accountId"] = accountId,
+                                ["create"] = new JObject
+                                {
+                                    ["new-masked-email"] = new JObject
+                                    {
+                                        ["state"] = "enabled",
+                                        ["description"] = description,
+                                        ["url"] = "",
+                                        ["emailPrefix"] = ""
+                                    }
+                                }
+                            },
+                            "0"
+                        }
+                    })
+            };
+            
+            var content = new StringContent(requestJObj.ToString(), Encoding.UTF8, "application/json");
+            
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri(apiUrl ?? string.Empty),
+                Content = content
+            };
+            
+            var maskedEmailMessage = await client.SendAsync(request);
+            maskedEmailMessage.EnsureSuccessStatusCode();
+            var maskedEmailResponse = await maskedEmailMessage.Content.ReadAsStringAsync();
+            
+            dynamic maskedEmailJson = JsonConvert.DeserializeObject(maskedEmailResponse) ?? throw new InvalidOperationException();
+            var maskedEmail = maskedEmailJson.methodResponses[0][1].created["new-masked-email"].email;
+            
+            return maskedEmail;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+}
